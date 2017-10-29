6eea68c9a6a8e14e1375437a35ec71c9636d8f51
chris48s <chris.shaw480@gmail.com>
2017-10-29 14:40:28 +0000

    add setup script and .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='commitment',
+    author="chris48s",
+    license="MIT",
+    url="https://github.com/chris48s/commitment/",
+    packages=['commitment'],
+    install_requires=[
+        'requests'
+    ],
+)
