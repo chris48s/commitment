426118340dbbd315537be018130af31dc3f02254
chris48s <chris.shaw480@gmail.com>
2018-05-07 13:11:19 +0100

    use markdown for long description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 def _get_description():
     try:
-        path = os.path.join(os.path.dirname(__file__), 'README.rst')
+        path = os.path.join(os.path.dirname(__file__), 'README.md')
         with open(path, encoding='utf-8') as f:
             return f.read()
     except IOError:
@@ -16,6 +16,7 @@ setup(
     license="MIT",
     description='An incomplete Python 3 wrapper for the GitHub API',
     long_description=_get_description(),
+    long_description_content_type="text/markdown",
     url="https://github.com/chris48s/commitment/",
     packages=['commitment'],
     install_requires=[
