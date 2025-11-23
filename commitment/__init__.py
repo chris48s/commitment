2d15477980cf59406c8352122446394c2616b6f4
chris48s <git@chris-shaw.dev>
2025-11-23 16:58:47 +0000

    add __version__

--- commitment/__init__.py
+++ commitment/__init__.py
@@ -1,1 +1,2 @@
-from .commitment import GitHubClient, GitHubCredentials  # noqa
+from .__version__ import __version__  # noqa: F401
+from .commitment import GitHubClient, GitHubCredentials  # noqa: F401
